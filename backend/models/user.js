4ce7b05f567da904920dfcd099b1bfa46071d326
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2025-05-17 16:01:32 -0600

    feat: modificación del modelo de usuario

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const urlRegex = /^https?:\/\/(www\.)?[\w\-._~:/?#[\]@!$&'()*+,;=%]+#?$/;
 
@@ -22,7 +23,22 @@ const userSchema = new mongoose.Schema({
       validator: (v) => urlRegex.test(v),
       message: 'El enlace del avatar no es válido'
     }
-  }
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'El formato del correo no es válido',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 16,
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
