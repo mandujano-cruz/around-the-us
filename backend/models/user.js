2d2dda6a8b24f0212324545afa132e960a29f5ce
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2025-05-18 15:19:20 -0600

    fix: se eviuta que api devuelva hash

--- backend/models/user.js
+++ backend/models/user.js
@@ -36,13 +36,14 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
     minlength: 6,
     maxlength: 16,
   },
 });
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if(!user) return Promise.reject(new Error ('Contraseña o correo electrónico incorrecto'));
       return bcrypt.compare(password, user.password)
