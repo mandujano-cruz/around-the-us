250413eb616fc51cfac50ffa7d24bd04179c9709
mandujano.cruz <jesus.mandujano.cruz@outlook.com>
2025-05-29 22:32:43 -0600

    fix: condición en error 500

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -2,9 +2,11 @@ module.exports.errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   console.error('Error capturado:', err);
 
-  res.status(statusCode).send({
+  if(err) {
+    res.status(statusCode).send({
     message: statusCode === 500
       ? 'Ha ocurrido un error en el servidor'
       : message,
   });
+  }
 };
